27cf72963f7fb00e9857fa5429f8dbc07a63bc9f
Edrian Mark Reyes <edrianmarkreyes1@gmail.com>
2026-05-05 17:32:57 +0800

    Revert Models/user.py

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@
 from extensions import db
 
 class User(db.Model):
-    _tablename_ = "users"
+    __tablename__ = "users"
     id              = db.Column(db.Integer,     primary_key=True)
     first_name      = db.Column(db.String(100), nullable=False)
     last_name       = db.Column(db.String(100), nullable=False)
